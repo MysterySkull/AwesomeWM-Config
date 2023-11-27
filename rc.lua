512590d21d3da029d53778480673f8b0e419013b
Guillaume Hernandez <guillaume.hernandez.1@gmail.com>
2023-11-27 17:44:30 +0100

    typos

--- rc.lua
+++ rc.lua
@@ -1,5 +1,9 @@
+--- Entry point of AwesomeWM.
+-- This configuration file is intended to change configuration only by changing general configuration name.
+-- Then, AwesomeWM will search for configuration file in the folder ./config/awesome/{general_config_name}
+
 --local general_config_name = "default"
 --local general_config_name = "initial_rice"
-local general_config_name = "wip_rice"
+general_config_name = "wip_rice"
 
 require(general_config_name .. "/rc")
