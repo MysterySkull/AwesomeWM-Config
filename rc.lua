48621f20795f857258fab9b8f52afb9d4d218217
Guillaume Hernandez <guillaume.hernandez.1@gmail.com>
2023-11-27 12:52:41 +0100

    make variable local

--- rc.lua
+++ rc.lua
@@ -1,5 +1,5 @@
---general_config_name = "default"
---general_config_name = "initial_rice"
-general_config_name = "wip_rice"
+--local general_config_name = "default"
+--local general_config_name = "initial_rice"
+local general_config_name = "wip_rice"
 
 require(general_config_name .. "/rc")
