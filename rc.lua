b5398834441fc44f1903a3e67d1de2ab2aa3b4ff
Guillaume Hernandez <guillaume.hernandez.1@gmail.com>
2023-12-02 15:22:31 +0100

    new rice to be configured

--- rc.lua
+++ rc.lua
@@ -2,8 +2,9 @@
 -- This configuration file is intended to change configuration only by changing general configuration name.
 -- Then, AwesomeWM will search for configuration file in the folder ./config/awesome/{general_config_name}
 
+local general_config_name = "current_rice"
 --local general_config_name = "default"
-general_config_name = "initial_rice"
--- general_config_name = "wip_rice"
+--general_config_name = "initial_rice"
+--general_config_name = "wip_rice"
 
 require(general_config_name .. "/rc")
