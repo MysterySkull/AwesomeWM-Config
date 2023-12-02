7d57ccfadcd92c799864c6ae8c5aec652eb0013e
Guillaume Hernandez <guillaume.hernandez.1@gmail.com>
2023-12-02 15:49:25 +0100

    Layout Module:w

--- current_rice/rc.lua
+++ current_rice/rc.lua
@@ -15,7 +15,6 @@ local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 
-
 --- GLOBAL VARIABLES
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
@@ -27,34 +26,13 @@ modkey = "Mod4"
 -- name of the theme
 rice_name = "current_rice"
 
--- Enable error handling
+--- ERROR HANDLING
 require(rice_name .."/error_handling")
 
-
--- Theme definition --
+--- THEME DEFINITION
 beautiful.init("~/.config/awesome/current_rice/theme.lua")
 
-
--- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    -- awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    -- awful.layout.suit.tile.top,
-    -- awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    -- awful.layout.suit.spiral,
-    -- awful.layout.suit.spiral.dwindle,
-    -- awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier,
-    -- awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
-}
--- }}}
+require(rice_name .."/layout")
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
